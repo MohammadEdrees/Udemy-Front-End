0db5bf7f6353572332388d6fcde005b42002a110
MohammadEdrees <medoenoch@yahoo.com>
2021-05-05 01:48:47 +0200

    collectV1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,7 @@ import { AppRoutingModule,routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {FontAwesomeModule} from '@fortawesome/angular-fontawesome'
 
-import { HomePageComponent } from './home-page/home-page.component';
-import { RegistrationStudentComponent } from './registration-student/registration-student.component';
-import { RegistrationInstructorComponent } from './registration-instructor/registration-instructor.component';
-import { LoginPageComponent } from './login-page/login-page.component';
 import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
-import { ProfileComponent } from './profile/profile.component';
-import { CategoryComponent } from './category/category.component';
-import { CourseComponent } from './course/course.component';
-import { UploadCourseComponent } from './upload-course/upload-course.component';
-
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
@@ -21,15 +12,8 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
   declarations: [
     AppComponent,
     routingComponents ,
-    HomePageComponent,
-    RegistrationStudentComponent,
-    RegistrationInstructorComponent,
-    LoginPageComponent,
     NavigationBarComponent,
-    ProfileComponent,
-    CategoryComponent,
-    CourseComponent,
-    UploadCourseComponent
+ 
   ],
   imports: [
     BrowserModule,

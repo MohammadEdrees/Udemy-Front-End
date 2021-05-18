4d491d3c5d0b90016f93b68801f428ef7d7158e1
MohammadEdrees <medoenoch@yahoo.com>
2021-05-18 21:53:29 +0200

    categoryComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { HomePageComponent } from './home-page/home-page.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { RegistrationInstructorComponent } from './registration-instructor/registration-instructor.component';
 import {RegistrationStudentComponent} from './registration-student/registration-student.component' 
+import {CategoryComponent}from './category/category.component'
 const routes: Routes = [
   {component:RegistrationStudentComponent,path:'studentreg'},
   {component:HomePageComponent,path:'home' } ,
   {component:LoginPageComponent,path:'login' } ,
   {component:RegistrationInstructorComponent,path:'instreg' } ,
+  {component:CategoryComponent,path:'category'},
   {path:"",redirectTo:"home",pathMatch:"full"}
 
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,15 @@ import { NavigationBarComponent } from './navigation-bar/navigation-bar.componen
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FooterComponent } from './footer/footer.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatMenuModule} from '@angular/material/menu'; 
+import {MatMenuModule} from '@angular/material/menu';
+import { CategoryComponent } from './category/category.component'; 
 @NgModule({
   declarations: [
     AppComponent,
     routingComponents ,
     NavigationBarComponent,
     FooterComponent,
+    CategoryComponent,
  
   ],
   imports: [
